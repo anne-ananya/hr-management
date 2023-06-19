5498c51f532236cb1f71ee7768e5cedbe231f80f
anne-ananya <124860809+anne-ananya@users.noreply.github.com>
2023-06-19 23:27:50 +0530

    Add files via upload

---
+++ Ap.jsx
@@ -0,0 +1,15 @@
+import './Ap.css'
+import BadgeForm from './Components/BadgeForm'
+import './Components/footer'
+import Footer from './Components/footer'
+
+function Ap() {
+
+  return (
+    <div className='App'>
+      <BadgeForm/><Footer/>
+    </div>
+  )
+}
+
+export default Ap
